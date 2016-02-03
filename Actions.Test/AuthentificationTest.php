42b8ec76cc40e9b398c822f58dfa40d2e7097504
anassqasmi <anass.qas@gmail.com>
2016-02-03 18:16:04 +0100

    update
    update

--- Actions.Test/AuthentificationTest.php
+++ Actions.Test/AuthentificationTest.php
@@ -1,10 +1,10 @@
 <?php
 
-//require 'Actions/Authentification.php';
+require 'Actions/Authentification.php';
 
-//require 'DAO/FileAccess.php';
+require 'DAO/FileAccess.php';
 
-//require 'Controlor/MainControlor.php';
+require 'Controlor/MainControlor.php';
 
 //test class
 class StackTest extends PHPUnit_Framework_TestCase
@@ -16,7 +16,11 @@ class StackTest extends PHPUnit_Framework_TestCase
 		//$this->assertEquals(0, count($stack));
 		//array_push($stack, 'foo');
 	
-		//$this->assertEquals('ok', readGroups2());
+		$this->assertEquals('ok', readGroups2());
+		$this->assertEquals('ok', readGroups2());
+		$this->assertEquals('ok', readGroups2());
+		$this->assertEquals('ok', readGroups2());
+		$this->assertEquals('ok', readGroups2());
 	
 	}
 	
@@ -26,8 +30,11 @@ class StackTest extends PHPUnit_Framework_TestCase
 		//$this->assertEquals(0, count($stack));
 		//array_push($stack, 'foo');
 		
-		//$this->assertEquals(true, test('anass', 'anass'));
-		
+		$this->assertEquals(true, test('anass', 'anass'));
+		$this->assertEquals(true, test('anass', 'anass'));
+			$this->assertEquals(true, test('anass', 'anass'));
+				$this->assertEquals(true, test('anass', 'anass'));
+					$this->assertEquals(true, test('anass', 'anass'));
 	}
 }
 //
