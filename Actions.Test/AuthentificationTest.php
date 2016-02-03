9ed9b42e6b3c376b7b20d3f240a21fd6e1e50855
anass <anass.qas@gmail.com>
2016-02-03 20:23:31 +0100

    update
    modification de bugs

--- Actions.Test/AuthentificationTest.php
+++ Actions.Test/AuthentificationTest.php
@@ -6,29 +6,47 @@ require 'DAO/FileAccess.php';
 
 require 'Controlor/MainControlor.php';
 
+$_POST['id'] = "1";
 //test class
 class StackTest extends PHPUnit_Framework_TestCase
 {
 	
-	public function GroupesTest()
+public function testGroupesTest()
 	{
-		//$stack = array();
-		//$this->assertEquals(0, count($stack));
-		//array_push($stack, 'foo');
-		
-		$this->assertEquals('ok', readGroups2());
-		
-	}
+		$this->assertEquals(true, test('anass', 'anass'));
 	
+	}
 	
 	public function test2()
 	{
-		//$stack = array();
-		//$this->assertEquals(0, count($stack));
-		//array_push($stack, 'foo');
-		
 		$this->assertEquals(true, test('anass', 'anass'));
-		
+	}
+	
+	public function testDeleteTags()
+	{
+		$this->assertEquals(true, deleteTag($_POST['id']));
+	}
+	
+	
+	
+	function deleteTag()
+	{
+		if (isset($_POST['id']))
+		{
+			return deleteTagg($_POST['id']);
+		}else {
+			return false;
+		}
+	}
+	
+	function deleteTagg($id)
+	{
+		if (!isset($_POST['id']))
+		{
+			return false;
+		}else{
+			return true;
+		}
 	}
 }
 
